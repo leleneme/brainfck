f47907854872e920edd58b437f6a02abb0b10480
leleneme <joaolemss@gmail.com>
2024-03-19 20:22:58 -0300

    c backend: cleanup and fixes

--- src/compiler_c.cpp
+++ src/compiler_c.cpp
@@ -14,15 +14,14 @@
 // be printed without any identation in a single line.
 #define C_BACKEND_PPRINT 1
 
-#if C_BACKEND_PPRINT
 static usize ident = 0;
-#endif
 
 namespace {
 
 template <class... Args>
 inline void print(std::format_string<Args...> fmt, Args&&... args) {
     std::string s = std::vformat(fmt.get(), std::make_format_args(args...));
+
 #if C_BACKEND_PPRINT
     for (usize i = 0; i < ident; i++) {
         putchar('\t');
@@ -36,34 +35,34 @@ inline void print(std::format_string<Args...> fmt, Args&&... args) {
 }
 
 void compiler_c::compile() {
+    // Using puts for headers to assure that it is not printed in the same line
+    // when C_BACKEND_PPRINT is 0
     puts("#include <stdio.h>");
     puts("#include <stddef.h>");
+
     print("char mem[30000] = {{0}};");
     print("size_t head = 0;");
     print("int main(void) {{");
 
-#if C_BACKEND_PPRINT
-    ident = 1;
-#endif
-
+    ident++;
     while (location < tokens.size()) {
         auto token = tokens[location];
         switch (token.type) {
         case token_type::add:
-            printf("mem[head]+=%zu;", token.operand);
+            print("mem[head]+={};", token.operand);
             break;
         case token_type::sub:
-            printf("mem[head]-=%zu;", token.operand);
+            print("mem[head]-={};", token.operand);
             break;
         case token_type::shift_left:
-            printf("head-=%zu;", token.operand);
+            print("head-={};", token.operand);
             break;
         case token_type::shift_right:
-            printf("head+=%zu;", token.operand);
+            print("head+={};", token.operand);
             break;
         case token_type::write:
             for (usize i = 0; i < token.operand; i++) {
-                printf("putchar(mem[head]);");
+                print("putchar(mem[head]);");
             }
             break;
         case token_type::read:
@@ -73,14 +72,10 @@ void compiler_c::compile() {
             break;
         case token_type::jmp_z:
             print("while(mem[head]){{");
-#if C_BACKEND_PPRINT
             ident++;
-#endif
             break;
         case token_type::jmp_nz:
-#if C_BACKEND_PPRINT
             ident--;
-#endif
             print("}}");
         default:
             break;
@@ -90,10 +85,6 @@ void compiler_c::compile() {
     }
 
     print("return 0;");
-
-#if C_BACKEND_PPRINT
-    ident = 0;
-#endif
-
+    ident--;
     print("}}");
 }
